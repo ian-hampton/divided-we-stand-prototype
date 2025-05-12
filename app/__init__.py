ea1434715eb13557e4d89b16a5f889135db598cf
ian-hampton <ian@ianhampton.com>
2025-05-11 22:53:09 -0400

    removed config stuff that was never used

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     
-    # Load configuration settings
-    # app.config.from_object('config.Config')
-    
-    # Register blueprints
+    # register blueprints
     from app.routes import main
     app.register_blueprint(main)
     
