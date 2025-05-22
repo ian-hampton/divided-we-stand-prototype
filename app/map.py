4c19d29148db48d8abe955ab30a52e9af2031656
ian-hampton <ian@ianhampton.com>
2025-05-21 21:56:05 -0400

    unit placement code draft

--- app/map.py
+++ app/map.py
@@ -1,7 +1,7 @@
 import json
 import random
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 from app import core
 from app import palette
@@ -9,6 +9,11 @@ from app.region import Region
 from app.improvement import Improvement
 from app.unit import Unit
 from app.nationdata import NationTable
+
+
+MAP_OPACITY = 0.75
+DO_NOT_SPAWN = {'Capital', 'Military Base', 'Missile Defense Network', 'Missile Silo', 'Oil Refinery',
+                'Research Institute', 'Surveillance Center', 'Nuclear Power Plant'}
 
 class GameMaps:
     
@@ -65,7 +70,6 @@ class GameMaps:
                     ImageDraw.floodfill(self.resource_map, tuple(coords), fill_color, border=(0, 0, 0, 255))
 
             # apply background texture
-            MAP_OPACITY = 0.75
             background_img = Image.open(self.filepath_background)
             background_img = background_img.convert("RGBA")
             self.main_map = Image.blend(background_img, self.main_map, MAP_OPACITY)
@@ -103,15 +107,51 @@ class GameMaps:
                 region = Region(region_id, self.game_id)
                 improvement = Improvement(region_id, self.game_id)
                 unit = Unit(region_id, self.game_id)
+                
+                if unit.name is not None:
+                    
+                    # place unit image
+                    nation = nation_table.get(str(region.occupier_id))
+                    fill_color = palette.hex_to_tup(nation.color, alpha=True)
+                    unit_img = Image.open(self.filepath_unit_back)
+                    ImageDraw.floodfill(unit_img, (1, 1), fill_color, border=(0, 0, 0, 255))
 
-                # place unit
-                pass
+                    # place unit symbol
+                    fill_color = palette.normal_to_occupied[nation.color]
+                    fill_color = palette.hex_to_tup(fill_color, alpha=True)
+                    symb_back_img = Image.open(self.filepath_unit_symb_back)
+                    ImageDraw.floodfill(symb_back_img, (1, 1), fill_color, border=(0, 0, 0, 255))
+                    symb_img = Image.open(f"{self.images_filepath}/units/{unit.name.lower()}.png")
+                    symb_img = Image.alpha_composite(symb_img, symb_back_img)
+                    unit_img.paste(symb_img, (9, 16))
 
-                # place nuclear explosion
-                pass
+                    # place unit name
+                    font = ImageFont.truetype("app/fonts/LeelawUI.ttf", size=12)
+                    ImageDraw.Draw(unit_img).text(xy=(24, 4), text=unit.abbrev(), fill=(0, 0, 0, 255), font=font, align="center")
+
+                    # place unit health
+                    max_health = unit_data_dict[unit.name]["Health"]
+                    ImageDraw.Draw(unit_img).text(xy=(24, 35), text=f"{unit.health}/{max_health}", fill=(0, 0, 0, 255), font=font, align="center")
+                    
+                    # place unit on map
+                    x = unit.coords[0]
+                    y = unit.coords[1]
+                    self.main_map.paste(unit_img, (x, y))
+
+                if region.fallout:
+                    # place nuclear explosion
+                    # TODO: make shadow blend properly
+                    mask = self.nuke_img.split()[3]
+                    self.main_map.paste(self.nuke_img, improvement.coords, mask)
+                    continue
             
-                # place improvement
-                pass
+                if improvement.name is not None:
+                    
+                    # place improvement image
+                    pass
+                    
+                    # place improvement health
+                    pass
 
             # save images
             match self.turn_num:
@@ -136,7 +176,6 @@ class GameMaps:
         
         # get list of improvements that can spawn
         improvement_candidates_list = []
-        DO_NOT_SPAWN = {'Capital', 'Military Base', 'Missile Defense Network', 'Missile Silo', 'Oil Refinery', 'Research Institute', 'Surveillance Center', 'Nuclear Power Plant'}
         for improvement_name in improvement_data_dict:
             if improvement_data_dict[improvement_name]['Required Resource'] == None and improvement_name not in DO_NOT_SPAWN:
                 improvement_candidates_list.append(improvement_name)
@@ -217,15 +256,20 @@ class GameMaps:
 
     def _init_images(self) -> None:
         
-        image_resources_filepath = f"app/static/images/map_images/{self.map_str}/image_resources"
-        self.filepath_background = f"{image_resources_filepath}/background.png"
-        self.filepath_magnified = f"{image_resources_filepath}/magnified.png"
-        self.filepath_main = f"{image_resources_filepath}/main.png"
-        self.filepath_text = f"{image_resources_filepath}/text.png"
+        map_images_filepath = f"app/static/images/map_images/{self.map_str}/image_resources"
+        self.filepath_background = f"{map_images_filepath}/background.png"
+        self.filepath_magnified = f"{map_images_filepath}/magnified.png"
+        self.filepath_main = f"{map_images_filepath}/main.png"
+        self.filepath_text = f"{map_images_filepath}/text.png"
         
         self.main_map = Image.open(self.filepath_main).convert("RGBA")
         self.resource_map = Image.open(self.filepath_main).convert("RGBA")
         self.control_map = Image.open(self.filepath_main).convert("RGBA")
+
+        self.images_filepath = "app/static/images"
+        self.filepath_unit_back = f"{self.images_filepath}/units/back.png"
+        self.filepath_unit_symb_back = f"{self.images_filepath}/units/back_symb.png"
+        self.nuke_img = Image.open(f"{self.images_filepath}/nuke.png")
 
     def _get_fill_color(self, nation_table: NationTable, region: Region) -> tuple | None:
         
