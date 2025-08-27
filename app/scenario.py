801ba4c22ce78e7e74fdf9498ab7f4deb6b12b85
ian-hampton <ian@ianhampton.com>
2025-08-27 13:44:33 -0400

    added capacity attribute

--- app/scenario.py
+++ app/scenario.py
@@ -19,6 +19,7 @@ class SD_Alliance:
     
     def __init__(self, d: dict):
         self.required_agenda: str = d["Required Agenda"]
+        self.capacity: bool = d.get("Capacity") is not None
         self.color_theme: str = d["colorTheme"]
         self.description: list = d["Description"]
 
