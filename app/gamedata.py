daef30a5f98a8d37450c2d7186ef285e846195c4
ian-hampton <ian@ianhampton.com>
2025-09-17 14:22:48 -0400

    added parent class

--- app/gamedata.py
+++ app/gamedata.py
@@ -5,59 +5,49 @@ import random
 from dataclasses import dataclass
 from typing import ClassVar
 
-class GameInformation:
-    pass
-
-class GameStatistics:
-    pass
-
 @dataclass
-class GameData:
+class Games:
 
-    id: ClassVar[str] = None
-    name: ClassVar[str] = None
-    number: ClassVar[str] = None
-    turn: ClassVar[int] = None
-    status: ClassVar[int] = None
-    
-    current_event: ClassVar[dict] = None
-    active_events: ClassVar[dict] = None
-    inactive_events: ClassVar[list] = None
-    
-    info: ClassVar[GameInformation] = None
-    stats: ClassVar[GameStatistics] = None
+    _data: ClassVar[dict[str, dict]] = None
     
     @classmethod
-    def load(cls, game_id: str) -> None:
-
-        cls.id = game_id
+    def load(cls) -> None:
 
         with open("active_games.json", 'r') as json_file:
-            active_games_dict = json.load(json_file)
-
-        cls._data = active_games_dict.get(cls.id)
-        if cls._data is None:
-            raise RuntimeError(f"Error: Game data not found for {cls.id} when attempting to initiate GameData class.")
-        
-        cls.name = cls._data["name"]
-        cls.number = cls._data["number"]
-        cls.turn = cls._data["turn"]
-        cls.status = cls._data["status"]
-        cls.current_event = cls._data["currentEvent"]
-        cls.active_events = cls._data["activeEvents"]
-        cls.inactive_events = cls._data["inactiveEvents"]
-        cls.info = GameInformation(cls._data["information"])
-        cls.stats = GameStatistics(cls._data["statistics"])
+            cls._data = json.load(json_file)
 
     @classmethod
     def save(cls) -> None:
 
         if cls._data is None:
-            raise RuntimeError("Error: GameData has not been loaded.")
+            raise RuntimeError("Error: Games dataclass has not been loaded.")
         
-        with open("active_games.json", 'r') as json_file:
-            active_games_dict = json.load(json_file)
+        with open("active_games.json", 'w') as json_file:
+            json.dump(cls._data, json_file, indent=4)
+
+class GameData:
+    
+    def __init__(self, game_id: str):
+
+        self._data = Games._data[game_id]
         
-        active_games_dict[cls.id] = cls._data
-        with open('active_games.json', 'w') as json_file:
-            json.dump(active_games_dict, json_file, indent=4)
+        self.id = game_id
+        self.name = self._data["name"]
+        self.number = self._data["number"]
+        self.turn = self._data["turn"]
+        self.status = self._data["status"]
+
+        self.current_event = self._data["currentEvent"]
+        self.active_events = self._data["activeEvents"]
+        self.inactive_events = self._data["inactiveEvents"]
+
+        self.info = GameInformation(self._data["information"])
+        self.stats = GameStatistics(self._data["statistics"])
+
+    # need to add getters and setters
+
+class GameInformation:
+    pass
+
+class GameStatistics:
+    pass
