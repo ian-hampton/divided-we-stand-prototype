f2c5ca0103944d60cfb4ee39f0c976998f034ef7
ian-hampton <ian@ianhampton.com>
2025-10-07 07:29:46 -0400

    updated truce decorators

--- app/truce.py
+++ app/truce.py
@@ -73,34 +73,30 @@ class Truces(metaclass=TrucesMeta):
 class Truce:
 
     def __init__(self, truce_id: str):
-
         self._data = Truces._data[truce_id]
-
         self.id = truce_id
         self.start_turn: int = self._data["startTurn"]
-        self._end_turn: int = self._data["endTurn"]
 
     @property
     def end_turn(self) -> int:
-        return self._end_turn
+        return self._data["endTurn"]
     
+    @end_turn.setter
+    def end_turn(self, turn: int) -> None:
+        self._data["endTurn"] = turn
+
     @property
     def signatories(self) -> dict[str, bool]:
         return self._data["signatories"]
     
+    @signatories.setter
+    def signatories(self, value: dict) -> None:
+        self._data["signatories"] = value
+
     @property
     def is_active(self) -> bool:
         game = Games.load(Truces.game_id)
         return True if self.end_turn > game.turn else False
-    
-    @end_turn.setter
-    def end_turn(self, turn: int) -> None:
-        self._end_turn = turn
-        self._data["endTurn"] = turn
-
-    @signatories.setter
-    def signatories(self, value: dict) -> None:
-        self._data["signatories"] = value
 
     def __str__(self):
         signatories_list = list(self.signatories.keys())
