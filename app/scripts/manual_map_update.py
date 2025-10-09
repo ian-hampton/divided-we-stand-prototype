3c7ac7d6b7fd4585ba05ae6c929011db7ea251ed
ian-hampton <ian@ianhampton.com>
2025-10-09 06:58:05 -0400

    applied ad-hoc patches from last game

---
+++ app/scripts/manual_map_update.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(parent_dir)
+os.chdir(parent_dir)
+
+from app.gamedata import Games
+from app.scenario import ScenarioData as SD
+from app.map import GameMaps
+from app.region import Regions
+from app.nation import Nations
+
+GAME_ID = "game1"
+
+game = Games.load(GAME_ID)
+SD.load(GAME_ID)
+Regions.load(GAME_ID)
+Nations.load(GAME_ID)
+
+print(f"Manually updating maps for game {GAME_ID}...")
+maps = GameMaps(GAME_ID)
+maps.update_all()
