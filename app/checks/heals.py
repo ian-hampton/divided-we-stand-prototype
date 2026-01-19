05495241c54580e82730cac3672d652a883e8be4
ian-hampton <ian@ianhampton.com>
2026-01-19 08:41:16 -0500

    finished new healing code

--- app/checks/heals.py
+++ app/checks/heals.py
@@ -3,7 +3,45 @@ from app.region.regions import Regions
 from app.war.wars import Wars
 
 def heal_units():
-    pass
+    """
+    Heals every unit that is eligible to be healed.
+    """
+
+    for region in Regions:
+
+        # check that unit exists
+        if region.unit.name is None:
+            continue
+
+        # do not heal units owned by an event player (99)
+        if region.data.owner_id == "99":
+            continue
+
+        # do not heal units that have been attacked this turn
+        if region.unit.has_been_attacked:
+            continue
+
+        unit_owner = Nations.get(region.unit.owner_id)
+        heal_allowed = False
+
+        # check if unit is allowed to heal based on its positioning
+        if region.unit.type == "Special Forces":
+            heal_allowed = True    # special forces can always heal
+        elif "Scorched Earth" in unit_owner.completed_research:
+            heal_allowed = True    # unit can always heal if Scorched Earth researched
+        elif region.data.owner_id == region.unit.owner_id:
+            heal_allowed = True    # unit can always heal within its own territory
+        else:
+            for adjacent_region in region.graph.iter_adjacent_regions():
+                if adjacent_region.unit.owner_id == region.unit.owner_id:
+                    heal_allowed = True    # unit can heal if located next to a friendly unit
+        
+        if not heal_allowed:
+            continue
+
+        region.unit.heal(1)
+        if "Peacetime Recovery" in unit_owner.completed_research and Wars.is_at_peace(unit_owner.id):
+            region.unit.heal(1)
 
 def heal_improvements():
     """
@@ -12,11 +50,16 @@ def heal_improvements():
     
     for region in Regions:
 
+        # check that improvement exists and has health
+        if region.improvement.name is None or region.improvement.health == 99:
+            continue
+
         # do not heal unowned improvements (0) or improvements owned by an event player (99)
         if region.data.owner_id in ["0", "99"]:
             continue
-        
-        if region.improvement.name is None or region.improvement.health == 99:
+
+        # do not heal improvements that have been attacked this turn
+        if region.improvement.has_been_attacked:
             continue
 
         region.improvement.heal(1)
