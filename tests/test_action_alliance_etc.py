3abeeb5606a0a687de92b410b20f5c62212650d6
ian-hampton <ian@ianhampton.com>
2026-01-30 07:49:06 -0500

    finished tests for alliance join and leave

--- tests/test_action_alliance_etc.py
+++ tests/test_action_alliance_etc.py
@@ -4,7 +4,6 @@ Author: Ian Hampton
 Created Date: 29th January 2026
 
 Comprehensive series of unit tests for all other alliance actions.
-See test_action_alliance_create.py for alliance creation tests.
 """
 
 import unittest
@@ -24,7 +23,10 @@ GAME_ID = "HrQyxUeblAMjTJbTrxsp"
 GAMEDATA_FILE = "tests/mock-files/gamedata.json"
 REGDATA_FILE = "tests/mock-files/regdata.json"
 
-class TestAllianceCreate(unittest.TestCase):
+ALLIANCE_NAME = "Test"
+ALLIANCE_TYPE = "Non-Aggression Pact"
+
+class TestAllianceEtc(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -66,9 +68,6 @@ class TestAllianceCreate(unittest.TestCase):
         with patch.object(Notifications, "_gamedata_path", return_value=str(GAMEDATA_FILE)):
             Notifications.load(GAME_ID)
 
-        ALLIANCE_NAME = "Test"
-        ALLIANCE_TYPE = "Non-Aggression Pact"
-
         # create and verify actions
         a1 = AllianceCreateAction(GAME_ID, "3", f"Alliance Create {ALLIANCE_NAME} as {ALLIANCE_TYPE}")
         assert a1.is_valid() == True
@@ -101,22 +100,120 @@ class TestAllianceCreate(unittest.TestCase):
         assert any(notification[1] == f"{ALLIANCE_NAME} has formed." for notification in Notifications)
 
     def test_join_simple(self):
-        pass
+        """
+        Simple alliance join action. Should pass.
+        """
+        from app.actions import AllianceJoinAction, resolve_alliance_join_actions
+
+        # create and verify action
+        a1 = AllianceJoinAction(GAME_ID, "2", f"Alliance Join {ALLIANCE_NAME}")
+        assert a1.is_valid() == True
+        assert a1.game_id == GAME_ID
+        assert a1.id == "2"
+        assert a1.alliance_name == ALLIANCE_NAME
+
+        # add agendas to nations
+        nation_b = Nations.get("2")
+        nation_b.completed_research["Common Ground"] = True
+
+        # execute actions
+        resolve_alliance_join_actions(GAME_ID, [a1])
+
+        # test alliance
+        alliance = Alliances.get(ALLIANCE_NAME)
+        assert alliance.name == ALLIANCE_NAME
+        assert alliance.is_active == True
+        assert alliance.current_members == {"Nation C": 33, "Nation D": 33, "Nation B": 33}
+        assert alliance.founding_members == {"Nation C": 33, "Nation D": 33}
+        assert any(notification[1] == f"Nation B has joined the {alliance.name}." for notification in Notifications)
+
+        # test nation b
+        assert f"Joined {alliance.name}." in nation_b.action_log
 
     def test_join_bad_research(self):
-        pass
+        """
+        Alliance join action that will fail due to player not having the correct agenda.
+        """
+        from app.actions import AllianceJoinAction, resolve_alliance_join_actions
+
+        # create and verify action
+        a1 = AllianceJoinAction(GAME_ID, "2", f"Alliance Join {ALLIANCE_NAME}")
+        assert a1.is_valid() == True
+        assert a1.game_id == GAME_ID
+        assert a1.id == "2"
+        assert a1.alliance_name == ALLIANCE_NAME
+
+        # add agendas to nations
+        nation_b = Nations.get("2")
+
+        # execute actions
+        resolve_alliance_join_actions(GAME_ID, [a1])
+
+        # test alliance
+        alliance = Alliances.get(ALLIANCE_NAME)
+        assert alliance.name == ALLIANCE_NAME
+        assert alliance.is_active == True
+        assert alliance.current_members == {"Nation C": 33, "Nation D": 33}
+        assert alliance.founding_members == {"Nation C": 33, "Nation D": 33}
+
+        # test nation b
+        assert f"Failed to join {ALLIANCE_NAME} alliance. You do not have the required agenda." in nation_b.action_log
 
     def test_join_blocked_by_status(self):
-        pass
+        """
+        Alliance join action that will fail due to player being a puppet state.
+        """
+        from app.actions import AllianceJoinAction, resolve_alliance_join_actions
+
+        # create and verify action
+        a1 = AllianceJoinAction(GAME_ID, "2", f"Alliance Join {ALLIANCE_NAME}")
+        assert a1.is_valid() == True
+        assert a1.game_id == GAME_ID
+        assert a1.id == "2"
+        assert a1.alliance_name == ALLIANCE_NAME
+
+        # add agendas to nations
+        nation_b = Nations.get("2")
+        nation_b.completed_research["Common Ground"] = True
+        nation_b.status = "Puppet State of Nation D"
+
+        # execute actions
+        resolve_alliance_join_actions(GAME_ID, [a1])
+
+        # test alliance
+        alliance = Alliances.get(ALLIANCE_NAME)
+        assert alliance.name == ALLIANCE_NAME
+        assert alliance.is_active == True
+        assert alliance.current_members == {"Nation C": 33, "Nation D": 33}
+        assert alliance.founding_members == {"Nation C": 33, "Nation D": 33}
+
+        # test nation b
+        assert f"Failed to join {ALLIANCE_NAME} alliance. Puppet states cannot join alliances." in nation_b.action_log
 
     def test_leave(self):
-        pass
+        """
+        Simple alliance leave action. Should pass.
+        """
+        from app.actions import AllianceLeaveAction, resolve_alliance_leave_actions
 
-    def test_kick(self):
-        pass
+        # create and verify action
+        a1 = AllianceLeaveAction(GAME_ID, "3", f"Alliance Leave {ALLIANCE_NAME}")
+        assert a1.is_valid() == True
+        assert a1.game_id == GAME_ID
+        assert a1.id == "3"
+        assert a1.alliance_name == ALLIANCE_NAME
 
-    def test_kick_bad_alliance(self):
-        pass
+        # execute actions
+        resolve_alliance_leave_actions(GAME_ID, [a1])
 
-    def test_kick_blocked_by_founder(self):
-        pass
+        # test alliance
+        alliance = Alliances.get(ALLIANCE_NAME)
+        assert alliance.name == ALLIANCE_NAME
+        assert alliance.is_active == True
+        assert alliance.current_members == {"Nation D": 33}
+        assert alliance.founding_members == {"Nation C": 33, "Nation D": 33}
+        assert any(notification[1] == f"Nation C has left the {alliance.name}." for notification in Notifications)
+
+        # test nation c
+        nation_c = Nations.get("3")
+        assert f"Left {ALLIANCE_NAME}." in nation_c.action_log
