cd17f288bc0d8f5c670738ae41ab238ab7147d68
ian-hampton <ian@ianhampton.com>
2026-09-12 10:22:06 -0400

    updated test cases

--- tests/test_combat.py
+++ tests/test_combat.py
@@ -261,8 +261,8 @@ class TestCombat(unittest.TestCase):
         assert NTEAZ.improvement.name == None
         assert NTEAZ.improvement.health == 99
         
-        # check NTEAZ XP    (2x destruction) + (2x occupation) = 4 + 4 = 8
-        assert NTEAZ.unit.xp == 8
+        # check NTEAZ XP    (2x destruction) + (2x occupation) = 4 + 2 = 6
+        assert NTEAZ.unit.xp == 6
 
     def test_in_vs_in_xp(self):
         """
